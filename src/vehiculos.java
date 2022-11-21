a6e2fe6713b8c37849d860cfaca8f3fdcb64c89f
DannyVinueza <danny.vinueza@epn.edu.ec>
2022-11-20 20:29:59 -0500

    Creacion de la clase vehiculos y del construtor.

--- src/vehiculos.java
+++ src/vehiculos.java
@@ -1,2 +1,16 @@
 public class vehiculos {
+    String marca;
+    String modelo;
+    int anio;
+    String color;
+    String tipo;
+
+    public void vehiculos(){
+        marca = "Chevrolet";
+        modelo = "Camaro";
+        anio = 2010;
+        tipo = "auto";
+    }
+
+
 }
