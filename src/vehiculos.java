0aef00e2dea74f7bd9f989744ea0dbec90e1ac21
DannyVinueza <danny.vinueza@epn.edu.ec>
2022-11-20 20:36:40 -0500

    Generacion de los Setter y Getter.

--- src/vehiculos.java
+++ src/vehiculos.java
@@ -12,5 +12,43 @@ public class vehiculos {
         tipo = "auto";
     }
 
+    public String getMarca() {
+        return marca;
+    }
 
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
